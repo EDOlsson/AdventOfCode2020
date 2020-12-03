059fd0b3888fb7fc774263816e16d3f3ef9a9792
Eric Olsson <edolsson@gmail.com>
2020-12-02 20:21:54 -0600

    Complete day 2 part 2

--- Program.fs
+++ Program.fs
@@ -6,9 +6,10 @@ let main argv =
     let day1Part1 = Day01.CalculatePart1
 
     // printfn "Day 1 part 1 : %d" day1Part1
+    // printfn "Day 1 part 2 : %d" <| Day01.CalculatePart2
 
-    // printfn "Day 2 part 2 : %d" <| Day01.CalculatePart2
-
-    printfn "Day 2 part 1 : %d" <| Day02.CalculatePart1
+    // printfn "Day 2 part 1 : %d" <| Day02.CalculatePart1
+    Day02.TestPart2 |> ignore
+    printfn "Day 2 part 2 : %d" <| Day02.CalculatePart2
 
     0
--- day02.fs
+++ day02.fs
@@ -42,3 +42,28 @@ let CalculatePart1 =
                   |> Array.length
 
     entries
+
+let private evaluateEntry2 entry =
+    let position1Matches = (entry.Password.Length >= entry.MinCount - 1) && (entry.Password.[entry.MinCount - 1] = entry.Letter)
+    let position2Matches = (entry.Password.Length >= entry.MaxCount - 1) && (entry.Password.[entry.MaxCount - 1] = entry.Letter)
+
+    (position1Matches && not position2Matches) || (not position1Matches && position2Matches)
+
+let CalculatePart2 =
+    let entries = readInput
+                  |> Array.choose parseEntry
+                  |> Array.map evaluateEntry2
+                  |> Array.filter id
+                  |> Array.length
+
+    entries
+
+let TestPart2 =
+    let testCases = [|
+        { MinCount = 1; MaxCount = 3; Letter = 'a'; Password = "abcde" }
+        { MinCount = 1; MaxCount = 3; Letter = 'b'; Password = "cdefg" }
+        { MinCount = 2; MaxCount = 9; Letter = 'c'; Password = "ccccccccc" } |]
+
+    printfn "1-3 a: abcde is %s" <| if evaluateEntry2 testCases.[0] then "VALID" else "INVALID"
+    printfn "1-3 b: cdefg is %s" <| if evaluateEntry2 testCases.[1] then "VALID" else "INVALID"
+    printfn "2-9 c: ccccccccc is %s" <| if evaluateEntry2 testCases.[2] then "VALID" else "INVALID"
